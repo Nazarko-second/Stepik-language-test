f1d8b94a411d258135991bcfd36ee90d6cee97e0
Nazar Dovhoshyya <nazarko.second@gmail.com>
2019-06-19 09:07:15 +0300

    REAMDE was deleted. Files CONFTEST and TEST_ITEMS has beem added

---
+++ test_items.py
@@ -0,0 +1,6 @@
+import time
+
+def test_add_to_cart_button_is_present(browser):
+	browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+	time.sleep(5)
+	assert browser.find_element_by_css_selector("button.btn-add-to-basket"), "No such button"
