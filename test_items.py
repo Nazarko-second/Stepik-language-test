7ec8b1da0aa213ddf793fdf1d80df4f18af99760
Nazar Dovhoshyya <nazarko.second@gmail.com>
2019-06-19 09:41:43 +0300

    3 sec wait added to test_items.py

--- test_items.py
+++ test_items.py
@@ -2,5 +2,5 @@ import time
 
 def test_add_to_cart_button_is_present(browser):
 	browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-	time.sleep(30)
+	time.sleep(3)
 	assert browser.find_element_by_css_selector("button.btn-add-to-basket"), "No such button"
